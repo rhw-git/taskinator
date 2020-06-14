99a7c378bd0f43fb49fe8edb15593d76ee1743da
rhw <ruohanw89@gmail.com>
2020-06-13 21:54:30 -0700

    separate functions

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,24 +2,40 @@
 var taskToDoEl = document.querySelector("#task-to-do");
 var formEl = document.querySelector("#task-form");
 
-var createTaskHandler = function(){
+var taskFormHandler = function(){
     event.preventDefault();
     
     var taskNameInput = document.querySelector("input[name='task-name']").value;
     var taskTypeInput = document.querySelector("select[name='task-type']").value;
 
-    // create list item
-    var listItemEl = document.createElement("li");
-    listItemEl.className = "task-item";
+    // package up data as an object
+    var taskDataObj = {
+        name: taskNameInput,
+        type: taskTypeInput
+    };
 
-    //create div to hold task info and add to list item
-    var taskInfoEl = document.createElement("div");
-    taskInfoEl.className = "task-info";
-    taskInfoEl.innerHTML = "<h3 class = 'task-name'>" + taskNameInput + "</h3> <span class = 'task-type'>" + taskTypeInput + "</span>";
-    listItemEl.appendChild(taskInfoEl);
-    taskToDoEl.appendChild(listItemEl);
+    // send it as argument to createTaskEl
+    createTaskEl(taskDataObj);
+}
+
+var createTaskEl = function(taskDataObj){
+    event.preventDefault();
+        // create list item
+        var listItemEl = document.createElement("li");
+        listItemEl.className = "task-item";
+    
+        //create div to hold task info and add to list item
+        var taskInfoEl = document.createElement("div");
+        taskInfoEl.className = "task-info";
+        taskInfoEl.innerHTML = "<h3 class = 'task-name'>" + taskDataObj.name + "</h3> <span class = 'task-type'>" + taskDataObj.type + "</span>";
+        
+        listItemEl.appendChild(taskInfoEl);
+        
+        //add entire list item to list
+        taskToDoEl.appendChild(listItemEl);
 }
 
 formEl.addEventListener ("submit",function(){
-       createTaskHandler();
-    }); 
+    taskFormHandler();
+    
+}); 
